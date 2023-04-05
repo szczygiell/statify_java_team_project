ead16cc04aea05e7273de274209598b8f7b176b3
jmacuga <julia.macuga@gmail.com>
2023-04-05 10:07:21 +0200

    get selected audio features

--- statify-app/src/main/java/com/statify/App.java
+++ statify-app/src/main/java/com/statify/App.java
@@ -13,10 +13,11 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        String user_token = "";
+        String user_token = "BQBa7O26g90UkKSzILPcTzyqhb0KOydJdpZQgRYZJU3eAqTi3V__pcZXmGvrlY7pFwqu_-0fA1mcMIFCZVimxMhshVGR2R0qK9iGiHdlr7zm_meMafHyljCJ0W91CfESJGfWQDX9fFC5NjgV7Y5bAHHAj63szDoXyeLhdSQacrzq3u5I7UQ2PFQblZbsCOWqm7W4ZSa3siCi_WYzjRCZ-tfwxPCVCW53H_278cCy9RkMeQubOMtSprpqAZdx9aGRKxve8kkeaCOVFOBy05PlxsdrlDuXMtVF3gLVp9G2HpadozNeutRNm2_4u2aoJffpudU";
         User user = new User(user_token);
         String playlistId = user.getFirstPlaylistId();
-        user.getPlaylistTracksIds(playlistId);
-
+        String trackId = user.getPlaylistTracksIds(playlistId).get(2);
+        // String tracks_albumId = user.getTracksAlbumId(trackId);
+        System.out.println(user.getTracksAudioFeatures(trackId));
     }
 }
--- statify-app/src/main/java/com/statify/User.java
+++ statify-app/src/main/java/com/statify/User.java
@@ -2,6 +2,8 @@ package com.statify;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -12,6 +14,12 @@ import se.michaelthelin.spotify.model_objects.specification.PlaylistSimplified;
 import se.michaelthelin.spotify.requests.data.playlists.GetListOfCurrentUsersPlaylistsRequest;
 import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
 import se.michaelthelin.spotify.model_objects.specification.PlaylistTrack;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.model_objects.specification.Album;
+import se.michaelthelin.spotify.requests.data.tracks.GetTrackRequest;
+import se.michaelthelin.spotify.requests.data.albums.GetAlbumRequest;
+import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
+import se.michaelthelin.spotify.requests.data.tracks.GetAudioFeaturesForTrackRequest;
 
 import java.io.IOException;
 
@@ -56,6 +64,7 @@ public class User {
         try {
             final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsItemsRequest.execute();
             List<String> tracks_indeces = new ArrayList<String>();
+            System.out.println(playlistTrackPaging.getItems()[0].getTrack().getName());
             for (PlaylistTrack pl_track : playlistTrackPaging.getItems()) {
                 tracks_indeces.add(pl_track.getTrack().getId());
             }
@@ -66,4 +75,36 @@ public class User {
         }
     }
 
+    public String getTracksAlbumId(String trackId) {
+        final GetTrackRequest getTrackRequest = spotifyApi.getTrack(trackId)
+                .build();
+        try {
+            final Track track = getTrackRequest.execute();
+            return track.getAlbum().getId();
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public Dictionary<String, Float> getTracksAudioFeatures(String trackId) {
+        final GetAudioFeaturesForTrackRequest getAudioFeaturesForTrackRequest = spotifyApi
+                .getAudioFeaturesForTrack(trackId)
+                .build();
+
+        Dictionary<String, Float> selectedAudioFeatures = new Hashtable<>();
+
+        try {
+            final AudioFeatures audioFeatures = getAudioFeaturesForTrackRequest.execute();
+            System.out.println("ID: " + audioFeatures.getId());
+            selectedAudioFeatures.put("dancebility", audioFeatures.getDanceability());
+            selectedAudioFeatures.put("loudness", audioFeatures.getLoudness());
+            selectedAudioFeatures.put("acousticness", audioFeatures.getAcousticness());
+            return selectedAudioFeatures;
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+            return selectedAudioFeatures;
+        }
+
+    }
 }
