8e3a4ee9a480c4012f59e0417c29ab5f5225c884
jmacuga <julia.macuga@gmail.com>
2023-05-21 23:50:15 +0200

    chrome browser opening with selenium

---
+++ statify-app/src/main/java/com/statify/Browser.java
@@ -0,0 +1,30 @@
+package com.statify;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Browser {
+
+    WebDriver driver;
+
+    public void setUp(String url) {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
+
+    public static void main(String[] args) {
+        Browser browser = new Browser();
+        String url = "http://www.google.pl";
+        browser.setUp(url);
+        try {
+
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        browser.driver.close();
+    }
+}
